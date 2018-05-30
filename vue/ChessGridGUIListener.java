d6ac03c07bb5b4913fe03ef9c1521c023763d90e
damienfern <damien.fernandes24@gmail.com>
2018-05-30 08:54:15 +0200

    First import

---
+++ vue/ChessGridGUIListener.java
@@ -0,0 +1,14 @@
+package vue;
+
+import java.util.EventListener;
+
+import controler.ChessGameControlerModelVue;
+
+public class ChessGridGUIListener implements EventListener {
+
+	public ChessGridGUIListener(ChessGridGUI chessGridGUI,
+			ChessGameControlerModelVue chessGameControler) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
